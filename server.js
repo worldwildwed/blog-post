2d934886dc7dbab5cb4e53ae2f952c7204a6a4d4
los-napath <flukeiedong@gmail.com>
2022-10-03 14:31:30 +0700

    mb

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+// about page
+app.get('/create', function(req, res) {
+    res.render('create');
+  });
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(4200);
+console.log('Server is listening on port 4200');
